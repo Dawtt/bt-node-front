6f85e6d30628bd3bbdfbac2988bfcb9c56bd90ba
dw <davidwhitebird@gmail.com>
2018-07-30 09:56:30 -0500

    save

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,16 +6,21 @@ import Counter from '../components/Counter'
 import NoMatch from '../components/NoMatch'
 import NavBar from '../components/NavBar'
 import AppSpotify from '../components/AppSpotify'
+import SpotifyLoginPage from '../components/SpotifyLoginPage'
 
 const routes = (
   <div>
     <NavBar />
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/hello" component={Hello} />
-      <Route path="/counter" component={Counter} />
-      <Route path="/spotifyStart" component={AppSpotify} />
-      <Route component={NoMatch} />
+        <Route exact path="/" component={Home} />
+        <Route path="/hello" component={Hello} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/spotifyStart" component={AppSpotify} />
+        <Route path="/spotify-redux-example" component={SpotifyLoginPage} />
+
+
+
+        <Route component={NoMatch} />
     </Switch>
   </div>
 )
