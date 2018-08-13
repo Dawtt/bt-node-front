179a4279b9d0b610abe9ffe20932adeb910095f0
dw <davidwhitebird@gmail.com>
2018-08-13 12:28:20 -0500

    autosearch: delete console.logs etc

--- src/Components/AutosearchArtist.js
+++ src/Components/AutosearchArtist.js
@@ -195,6 +195,9 @@ function MultiValue(props) {
 
 
 
+
+
+
 const components = {
     Option,
     Control,
@@ -212,49 +215,37 @@ class IntegrationReactSelect extends React.Component {
         artistlist: {},
     };
 
-    componentDidMount() {
+
+
+    fetchTheArtistList() {
         console.log("componenentdidMount() called, about to fetch");
         fetch("http://127.0.0.1:5000/getartistlist/")
             .then(console.log("past the fetch"))
             .then(res => res.json())
-  /*          .then((result) => {
-                console.log(result)
-            })*/
-
-
             .then(
                 (result) => {
-                    // console.log(result)
-                    console.log("here is test data")
-                    console.log(typeof result)
-                    console.log("type for suggestions: ")
-                    console.log(typeof suggestions)
-                    console.log(result[3])
-                    console.log(result[3].artist)
-                    console.log('artistlisttest before map')
-                    console.log(artistlisttest)
-                    console.log('artistlisttest after map')
                     artistlisttest = result.map(suggestion => ({
                         value: suggestion.artist,
                         label: suggestion.artist,
                     }));
-                    console.log(artistlisttest)
-                    this.setState({
-                        isLoaded: true,
-                        artistlist: artistlisttest,
-                    });
-                },
 
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
+                })
+    }
+
+    setArtistListState() {
+        console.log("before this.setState")
+
+        this.setState({
+            isLoaded: true,
+            artistlist: artistlisttest,
+        });
+        console.log("after this.setSTate")
+    }
+
+    componentDidMount() {
+        fetchTheArtistList();
+        setArtistListState();
+        console.log("component did mount comint to an end")
     }
 
     handleChange = name => value => {
